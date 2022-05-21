36f68a94d9f503bc7b8d6b74b777118b4dd82f3b
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-20 23:58:04 -0400

    thoughts query working

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,31 @@
+// import gql tagged template function
+
+const { gql } = require('apollo-server-express');
+
+//create typeDefs
+
+const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    thoughts(username: String): [Thought]
+  }
+`;
+
+//export typeDefs
+
+module.exports = typeDefs;
---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+//import Apollo, typeDefs, resolvers
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// create new apollo instance with GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
