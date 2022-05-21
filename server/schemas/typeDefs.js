3ed6c05cc00f108d82d104ce33f2d323bb7d9b61
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-21 01:00:16 -0400

    query for users and thoughts

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,15 +5,7 @@ const { gql } = require('apollo-server-express');
 //create typeDefs
 
 const typeDefs = gql`
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-
+  
   type Reaction {
     _id: ID
     reactionBody: String
@@ -21,9 +13,31 @@ const typeDefs = gql`
     username: String
   }
 
-  type Query {
-    thoughts(username: String): [Thought]
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  } 
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
   }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
 `;
 
 //export typeDefs
